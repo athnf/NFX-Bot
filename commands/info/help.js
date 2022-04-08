2d9caa0fe11467c0379a7330be257a1d64728fab
ThePrezz <70727135+ThePrezz@users.noreply.github.com>
2022-04-08 21:41:24 +0700

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,109 @@
+const { MessageEmbed } = require("discord.js");
+const { readdirSync } = require("fs");
+const prefix = require("../../config.json").prefix;
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Shows all available bot commands.",
+  run: async (client, message, args) => {
+    const msg = await message.channel.send(`🏓 Process...`);
+    const roleColor =
+      message.guild.me.displayHexColor === "30AADD"
+        ? "#ffffff"
+        : message.guild.me.displayHexColor;
+
+    if (!args[0]) {
+      let categories = [];
+
+      readdirSync("./commands/").forEach((dir) => {
+        const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
+          file.endsWith(".js")
+        );
+
+        const cmds = commands.map((command) => {
+          let file = require(`../../commands/${dir}/${command}`);
+
+          if (!file.name) return "No command name.";
+
+          let name = file.name.replace(".js", "");
+
+          return `\`${name}\``;
+        });
+
+        let data = new Object();
+
+        data = {
+          name: dir.toUpperCase(),
+          value: cmds.length === 0 ? "In progress." : cmds.join(" "),
+        };
+
+        categories.push(data);
+      });
+
+      const embed = new MessageEmbed()
+        .setTitle(
+          "📬 Butuh Bantuan? Untuk List Command Silahkan Cek Dibawah Ini!"
+        )
+        .addFields(categories)
+        .setDescription(
+          `Gunakan \`${prefix}help\` diikuti dengan nama perintah untuk mendapatkan lebih banyak informasi tambahan tentang suatu perintah. Sebagai Contoh: \`${prefix}help ban\`.`
+        )
+        .setFooter(
+          `Request by ${message.author.tag}`,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .setTimestamp()
+        .setColor(roleColor);
+      return message.channel.send(embed);
+    } else {
+      const command =
+        client.commands.get(args[0].toLowerCase()) ||
+        client.commands.find(
+          (c) => c.aliases && c.aliases.includes(args[0].toLowerCase())
+        );
+
+      if (!command) {
+        const embed = new MessageEmbed()
+          .setTitle(
+            `Invalid command! Gunakan \`${prefix}help\` Untuk Menunjukin Semua Commands!`
+          )
+          .setColor("FF0000");
+        return message.channel.send(embed);
+      }
+
+      const embed = new MessageEmbed()
+        .setTitle("Command Details:")
+        .addField("PREFIX:", `\`${prefix}\``)
+        .addField(
+          "COMMAND:",
+          command.name ? `\`${command.name}\`` : "No name for this command."
+        )
+        .addField(
+          "ALIASES:",
+          command.aliases
+            ? `\`${command.aliases.join("` `")}\``
+            : "No aliases for this command."
+        )
+        .addField(
+          "USAGE:",
+          command.usage
+            ? `\`${prefix}${command.name} ${command.usage}\``
+            : `\`${prefix}${command.name}\``
+        )
+        .addField(
+          "DESCRIPTION:",
+          command.description
+            ? command.description
+            : "No description for this command."
+        )
+        .setFooter(
+          `Requested by ${message.author.tag}`,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .setTimestamp()
+        .setColor(roleColor);
+      return message.channel.send(embed);
+    }
+  },
+};
